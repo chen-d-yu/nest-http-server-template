1b6ad92d9721e7afffb34ebf25d75a8f250e5347
chendiyu <cdy1998cdy@163.com>
2024-01-08 10:59:25 +0800

    feat: 补充basic-routing的HelloWorld案例

---
+++ src/user/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import * as Mock from 'mockjs';
+
+@Injectable()
+export class UserService {
+  findAll() {
+    return Mock.mock({
+      'list|10': [
+        {
+          'id|+1': 1,
+          name: '@cname',
+          email: '@email',
+        },
+      ],
+    });
+  }
+}
