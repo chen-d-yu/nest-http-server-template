46a9ca86e31d0f08f31d1a8638e3c4a3294628f6
zuoyou <cdy1998cdy@163.com>
2024-02-03 22:39:33 +0800

    feat：管道案例

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -12,7 +12,6 @@ export class ValidationPipe implements PipeTransform {
     // 将对象转换为 Class 来验证
     const object = plainToClass(metatype, value);
     const errors = await validate(object);
-    console.log(errors);
     if (errors.length > 0) {
       const msg = Object.values(errors[0].constraints)[0]; // 只需要取第一个错误信息并返回即可
       throw new BadRequestException(`Validation failed: ${msg}`);
